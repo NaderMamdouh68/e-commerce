25723e318dbe9207ea1d64057b0836adb74225c0
mikgit <mikelhfzy@gmail.com>
2023-03-29 15:49:14 +0200

    user

--- Frontend/src/pages/Dash/userList/UesrList.jsx
+++ Frontend/src/pages/Dash/userList/UesrList.jsx
@@ -1,54 +1,77 @@
 import React from 'react'
+import {Link, link} from 'react-router-dom'
 import './userlist.css'
-import { DataGrid } from '@mui/x-data-grid';
+import {DeleteOutline} from '@mui/icons-material'
+import { DataGrid, renderActionsCell } from '@mui/x-data-grid';
+import Avatar from "@mui/material/Avatar";
+import { userrows } from '../../../dummyData/dummyData';
 
 const UesrList = () => {
-
+  const handleDelete =() =>{
+    console.log("ok")
+  }
   const columns = [
-    { field: 'id', headerName: 'ID', width: 50 },
-    { field: 'firstName', headerName: 'First name', width: 130 },
-    { field: 'lastName', headerName: 'Last name', width: 130 },
+    { field: "id", headerName: "ID", width: 70 },
     {
-      field: 'age',
-      headerName: 'Age',
-      type: 'number',
-      width: 90,
+      field: "username",
+      headerName: "User",
+      width: 100,
     },
     {
-      field: 'fullName',
-      headerName: 'Full name',
-      description: 'This column has a value getter and is not sortable.',
-      sortable: false,
-      width: 160,
-      valueGetter: (params) =>
-        `${params.row.firstName || ''} ${params.row.lastName || ''}`,
+      field: 'avatar',
+      headerName: 'avatar',
+      width: 60,
+      editable: true,
+      renderCell: (params) => <Avatar src={params.value}/> // renderCell will render the component
     },
+    { 
+      field: "email",
+      headerName: "E-mail",
+      width: 150
+    },
+    {
+      field: "status",
+      headerName: "Status",
+      width: 90
+    },
+    {
+      field: "phone",
+      headerName: "phone",
+      width: 150
+    },
+    {
+      field: "actions",
+      headerName: "actions",
+      width: 150,
+      renderCell:(params)=>{
+        return(
+        <>
+          <Link to={"/user/"+params.row.id}>
+            <button className='edit'>Edit</button>
+          </Link>
+          
+          <DeleteOutline className='delete' onClick={handleDelete}/>
+        </>
+        )
+      }
+    }
+
   ];
   
   const rows = [
-    { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-    { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-    { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-    { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-    { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-    { id: 6, lastName: null, firstName: 'Melisandre', age: 150 },
-    { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-    { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-    { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+    { id: 1, 
+    username: 'Harry Potter',
+    avatar:"https://assets.materialup.com/uploads/bebad102-7f40-4941-99cd-54366113003e/avatar-08.png", 
+    email:"Harry@gmail.com", 
+    status:"Active",
+    phone: '01124354678',
+    },
   ];
 
 
   return (
-    <div className='userList'>
-      <div style={{ height: '100%', width: '100%' }}>
-      <DataGrid
-        rows={rows}
-        columns={columns}
-        pageSize={5}
-        rowsPerPageOptions={[5]}
-        checkboxSelection
-      />
-    </div>
+    <div className="userList">
+    <DataGrid rows={userrows} columns={columns} pageSize={20} disableRowSelectionOnClick rowsPerPageOptions={[5]} checkboxSelection />
     </div>
   )
 }
