c0372199b806eeed2823d2c11fb55f1bedfff953
omnia <omniaabdellatef193@gmail.com>
2023-04-02 16:47:22 +0200

    omnia

--- Server/controller/orderCRUD.js
+++ Server/controller/orderCRUD.js
@@ -5,10 +5,82 @@ import db from "./DB_Con.js";
 
 
 const Order = express();
-Category.use(cors());
-Category.use(bodyParser.urlencoded({ extended: false }));
-Category.use(bodyParser.json());
+Order.use(cors());
+Order.use(bodyParser.urlencoded({ extended: false }));
+Order.use(bodyParser.json());
 
+ Order.get('/', (req, res) => {
+    const sqlSelect = "SELECT * FROM order INNER JOIN product ON order.product_id  = product.product_id INNER JOIN user ON order.user_id = user.user_id ";
+    db.query(sqlSelect, (err, result) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.json(result);
+        }
+    });
+});
+
+Order.post('/ordercreate',(req, res) => {
+    const sqlInsert = "INSERT INTO order ( user_id, product_id , order_date)VALUES (?,?,?)";
+    const values = [req.body.user_id, req.body.product_id, req.body.order_date ];
+    db.query(sqlInsert, values, (err, result) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.json(result);
+        }
+    });
+});
+
+Order.get('/readorder/:id', (req, res) => {
+//     const sqlRead = "select order.order_id, order.user_id , order.product_id, order.order_date from order inner join //category on product.category_id = category.category_id where order_id  = ?";
+     const values = [req.params.id];
+     db.query(sqlRead, values, (err, result) => {
+         if (err) {
+            console.log(err);
+        } else {
+           res.json(result);
+         }
+     });
+ });
+
+// Products.get('/productfilter/:id', (req, res) => {
+//     const sqlRead = "select category.category_name, product.product_id, product.product_name, product.price, product.description, product.image from product inner join category on product.category_id = category.category_id where category.category_id = ?";
+//     const values = [req.params.id];
+//     db.query(sqlRead, values, (err, result) => {
+//         if (err) {
+//             console.log(err);
+//         } else {
+//             res.json(result);
+//         }
+//     });
+// });
+
+Order.put('/orderUpdate/:id',(req, res) => {
+    const sqlUpdate = "UPDATE order SET user_id = ?, product_id = ?, order_date = ?  WHERE order_id  = ?";
+    const values = [req.body.user_id , req.body.product_id, req.body.order_date ,req.params.id];
+    db.query(sqlUpdate, values, (err, result) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.json(result);
+        }
+    });
+});
+
+
+
+Order.delete('/orderdelete/:id', (req, res) => {
+    const sqlDelete = "DELETE FROM order WHERE order_id  = ?";
+    const values = [req.params.id];
+    db.query(sqlDelete, values, (err, result) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.json(result);
+        }
+    });
+});
 
 
 
