4a4a452fcd5caf551da11206dae1d247be4cae11
Nader Mamdouh <74846059+NaderMamdouh68@users.noreply.github.com>
2023-04-02 23:35:49 +0200

    nader

--- Server/controller/orderCRUD.js
+++ Server/controller/orderCRUD.js
@@ -10,7 +10,7 @@ Order.use(bodyParser.urlencoded({ extended: false }));
 Order.use(bodyParser.json());
 
  Order.get('/', (req, res) => {
-    const sqlSelect = "SELECT order_id, user_id, product_id, order_date , product.product_name, user.user_name FROM order INNER JOIN USING (user_id) INNER JOIN product USING (product_id)";
+    const sqlSelect = "SELECT order_id ,user_name, product_name, order_date FROM orders";
     db.query(sqlSelect, (err, result) => {
         if (err) {
             console.log(err);
@@ -22,8 +22,8 @@ Order.use(bodyParser.json());
 });
 
 Order.post('/ordercreate',(req, res) => {
-    const sqlInsert = "INSERT INTO order ( user_id, product_id , order_date)VALUES (?,?,?)";
-    const values = [req.body.user_id, req.body.product_id, req.body.order_date ];
+    const sqlInsert = "INSERT INTO orders ( user_id, user_name, product_id, product_name )VALUES (?,?,?,?)";
+    const values = [req.body.user_id,req.body.user_name, req.body.product_id, req.body.product_name ];
     db.query(sqlInsert, values, (err, result) => {
         if (err) {
             console.log(err);
@@ -34,7 +34,7 @@ Order.post('/ordercreate',(req, res) => {
 });
 
 Order.get('/readorder/:id', (req, res) => {
-//     const sqlRead = "select order.order_id, order.user_id , order.product_id, order.order_date from order inner join //category on product.category_id = category.category_id where order_id  = ?";
+     const sqlRead = "SELECT order_id ,user_name, product_name, order_date FROM orders where order_id = ?";
      const values = [req.params.id];
      db.query(sqlRead, values, (err, result) => {
          if (err) {
@@ -57,22 +57,9 @@ Order.get('/readorder/:id', (req, res) => {
 //     });
 // });
 
-Order.put('/orderUpdate/:id',(req, res) => {
-    const sqlUpdate = "UPDATE order SET user_id = ?, product_id = ?, order_date = ?  WHERE order_id  = ?";
-    const values = [req.body.user_id , req.body.product_id, req.body.order_date ,req.params.id];
-    db.query(sqlUpdate, values, (err, result) => {
-        if (err) {
-            console.log(err);
-        } else {
-            res.json(result);
-        }
-    });
-});
-
-
 
 Order.delete('/orderdelete/:id', (req, res) => {
-    const sqlDelete = "DELETE FROM order WHERE order_id  = ?";
+    const sqlDelete = "DELETE FROM orders WHERE order_id  = ?";
     const values = [req.params.id];
     db.query(sqlDelete, values, (err, result) => {
         if (err) {
